457ec9e99449bbc32485c6b6281a78a697e6f1a1
MrVineetRaj <vineetrajrj26@gmail.com>
2024-07-16 15:14:01 +0530

    Trying to run on vercel

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def transcribe_audio():
         return jsonify(result)
 
 
-if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))  # Default to 5000 if PORT environment variable is not set
-    app.run(debug=True, host='0.0.0.0', port=5000)
-    # app.run()
+# if __name__ == '__main__':
+#     port = int(os.getenv('PORT', 5000))  # Default to 5000 if PORT environment variable is not set
+#     app.run(debug=True, host='0.0.0.0', port=5000)
+#     # app.run()
